a9edc91c98827942b8c269630fd2b3992744dbf7
jiyoungeeeeeeeeeeeeeeeeeeeee <ghrn123123@gmail.com>
2024-11-01 14:18:54 +0900

    pipline-test

--- app/main.py
+++ app/main.py
@@ -4,10 +4,9 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():    return {"Hello": "MLOps"}
+def read_root():    return {"Hello": "MLOps!!!!!!!!!!!!!"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
-
